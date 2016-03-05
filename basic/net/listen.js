1cf0d66786e15a5ae8fdbc597018959dcf54ec01
Tian <tyybjcc@gmail.com>
2016-03-05 17:23:34 +0800

    Update listen.js

--- basic/net/listen.js
+++ basic/net/listen.js
@@ -1,1 +1,16 @@
-var net = require.js
+var net = require('net');
+
+var server = net.createServer(function(socket){
+  console.log('A new client connected...');
+});
+// instend: server.on('connection',function(socket){});
+
+
+server.listen(8899, 'localhost');
+//  server.on('listening',function(){});
+
+server.on('error', function(e) {
+  if(e.code == 'EADDRINUSE') {
+    console.log('listening port has already been used!');
+  }
+})
