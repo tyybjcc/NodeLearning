8ab1047feb1c8d5ea4bd7a1da88824756ca3853f
Tian <tyybjcc@gmail.com>
2016-03-05 18:37:39 +0800

    Update socket.js

--- basic/net/socket.js
+++ basic/net/socket.js
@@ -1,1 +1,20 @@
-
+/**
+ * net.Socket
+ * socket.address()
+ * socket.setEncoding('utf8')
+ * socket.bytesRead
+ * 
+ * */
+ 
+ var net = require('net');
+ var server = net.createServer();
+ server.on('connection', function(socket){
+   console.log(socket.address());
+   socket.setEncoding('utf8');
+   socket.on('data',function(data) {
+	   console.log(data);
+	   console.log(socket.bytesRead);
+   });
+ });
+ 
+ server.listen(8899,'localhost');
